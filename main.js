11d850469f419089c113bacbfb0b75fd2d4b96df
wonhuem-baek <74481706+wonhuem-baek@users.noreply.github.com>
2021-04-16 12:16:03 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,54 +3,54 @@ const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
 
 function onAdd() {
-    const text = input.value;
-    if(text === '') {
-        input.focus();
-        return;
-    }
-    const item = createItem(text);
-    items.appendChild(item);
-    item.scrollIntoView({block : 'center'});
-    input.value = '';
-    input.focus();    
+  const text = input.value;
+  if (text === '') {
+    input.focus();
+    return;
+  }
+
+  const item = createItem(text);
+  items.appendChild(item);
+  item.scrollIntoView({ block: 'center' });
+  input.value = '';
+  input.focus();
 }
 
 function createItem(text) {
-    const itemRow = document.createElement('li');
-    itemRow.setAttribute('class','item__row');
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
 
-    const item = document.createElement('div');
-    item.setAttribute('class','item');
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
 
-    const name = document.createElement('span');
-    name.setAttribute('class','item__name');
-    name.innerText = text;
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item__name');
+  name.innerText = text;
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class','item__delete');
-    deleteBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'item__delete');
+  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  deleteBtn.addEventListener('click', () => {
+    itemRow.remove();
+  });
 
-    deleteBtn.addEventListener('click', () => {
-        items.removeChild(itemRow);
-    });
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item__divider');
 
-    const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class','item__divider');
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
 
-    item.appendChild(name);
-    item.appendChild(deleteBtn);
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
-
-    return itemRow;
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
 }
 
-addBtn.addEventListener('click',() => {
-    onAdd();
+addBtn.addEventListener('click', () => {
+  onAdd();
 });
 
-input.addEventListener('keypress', (e) => {
-    if(e.key === 'Enter') {
-        onAdd();
-    }
+input.addEventListener('keypress', event => {
+  if (event.key === 'Enter') {
+    onAdd();
+  }
 });
